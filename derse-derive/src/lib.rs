dfe01f5a1682b48634f8b0e9121a96fb23bde363
SF-Zhou <sfzhou.scut@gmail.com>
2024-04-23 17:14:28 +0800

    support unnamed struct (#1)

--- derse-derive/src/lib.rs
+++ derse-derive/src/lib.rs
@@ -9,40 +9,68 @@ pub fn derse_derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
     let name = &ast.ident;
-    let mut field_types = Vec::new();
+    let mut serialize_fields = Vec::new();
+    let mut deserialize_fields = Vec::new();
 
-    let fields_tokens = if let Data::Struct(DataStruct {
-        fields: Fields::Named(fields),
-        ..
-    }) = ast.data
-    {
-        fields
-            .named
-            .iter()
-            .map(|f| {
+    let deserialize_statement = match ast.data {
+        Data::Struct(DataStruct {
+            fields: Fields::Named(fields),
+            ..
+        }) => {
+            for f in fields.named {
                 let field_name = &f.ident;
                 let field_type = &f.ty;
-                field_types.push(field_name.as_ref().unwrap().to_owned());
-                quote! {
+                serialize_fields.push(quote! {
+                    self.#field_name.serialize_to(serializer);
+                });
+                deserialize_fields.push(quote! {
                     #field_name: if buf.is_empty() {
                         #field_type::default()
                     } else {
                         #field_type::deserialize_from(&mut buf)?
                     },
+                });
+            }
+            quote! {
+                Self {
+                    #(#deserialize_fields)*
                 }
-            })
-            .collect::<Vec<_>>()
-    } else {
-        panic!("only named fields supported")
+            }
+        }
+        Data::Struct(DataStruct {
+            fields: Fields::Unnamed(fields),
+            ..
+        }) => {
+            for (i, f) in fields.unnamed.iter().enumerate() {
+                let index = syn::Index::from(i);
+                let field_type = &f.ty;
+                serialize_fields.push(quote! {
+                    self.#index.serialize_to(serializer);
+                });
+                deserialize_fields.push(quote! {
+                    if buf.is_empty() {
+                        #field_type::default()
+                    } else {
+                        #field_type::deserialize_from(&mut buf)?
+                    },
+                });
+            }
+            quote! {
+                Self (
+                    #(#deserialize_fields)*
+                )
+            }
+        }
+        _ => panic!("only struct is supported"),
     };
 
-    field_types.reverse();
+    serialize_fields.reverse();
 
     let gen = quote! {
         impl<'a> ::derse::Serialization<'a> for #name {
             fn serialize_to<T: ::derse::Serializer>(&self, serializer: &mut T) {
                 let start = serializer.len();
-                #(self.#field_types.serialize_to(serializer);)*
+                #(#serialize_fields)*
                 let len = serializer.len() - start;
                 ::derse::VarInt64(len as u64).serialize_to(serializer);
             }
@@ -63,9 +91,7 @@ pub fn derse_derive(input: TokenStream) -> TokenStream {
                 *buf = &buf[len..];
 
                 let mut buf = current;
-                Ok(Self {
-                    #(#fields_tokens)*
-                })
+                Ok(#deserialize_statement)
             }
         }
     };
--- derse/tests/test_derive.rs
+++ derse/tests/test_derive.rs
@@ -1,17 +1,30 @@
 use derse::{Derse, DownwardBytes, Serialization};
 
-#[derive(Debug, Derse, PartialEq)]
-struct A {
-    x: u64,
-    y: String,
-}
-
 #[test]
-fn test_struct() {
+fn test_named_struct() {
+    #[derive(Debug, Derse, PartialEq)]
+    struct A {
+        x: u64,
+        y: String,
+    }
+
     let ser = A {
         x: u64::MAX,
         y: "hello derse!".to_owned(),
     };
+    let bytes = ser.serialize::<DownwardBytes>();
+    assert_eq!(bytes.len(), 1 + 8 + 1 + 12);
+
+    let der = A::deserialize(&bytes).unwrap();
+    assert_eq!(ser, der);
+}
+
+#[test]
+fn test_unnamed_struct() {
+    #[derive(Debug, Derse, PartialEq)]
+    struct A(u32, u64, String);
+
+    let ser = A(u32::MAX, u64::MAX, "hello derse!".to_owned());
     let bytes = ser.serialize::<DownwardBytes>();
     assert_eq!(bytes.len(), 1 + 8 + 1 + 12);
 
