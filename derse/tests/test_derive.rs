6bf3d8e3939b45a749896ed831bac4a99c3420a4
SF-Zhou <sfzhou.scut@gmail.com>
2024-04-24 09:28:47 +0800

    add compatibility test

--- derse/tests/test_derive.rs
+++ derse/tests/test_derive.rs
@@ -31,3 +31,33 @@ fn test_unnamed_struct() {
     let der = A::deserialize(&bytes).unwrap();
     assert_eq!(ser, der);
 }
+
+#[test]
+fn test_compatibility() {
+    #[derive(Debug, Derse, PartialEq)]
+    struct A1(String, u64);
+
+    #[derive(Debug, Derse, PartialEq)]
+    struct A2(String, u64, String); // add a field.
+
+    {
+        let ser = A1("hello derse!".to_owned(), 12138);
+        let bytes = ser.serialize::<DownwardBytes>();
+        assert_eq!(bytes.len(), 1 + 1 + 12 + 8);
+
+        let der = A2::deserialize(&bytes).unwrap();
+        assert_eq!(ser.0, der.0);
+        assert_eq!(ser.1, der.1);
+        assert!(der.2.is_empty());
+    }
+
+    {
+        let ser = A2("hello derse!".to_owned(), 12138, "more data".to_owned());
+        let bytes = ser.serialize::<DownwardBytes>();
+        assert_eq!(bytes.len(), 1 + 1 + 12 + 8 + 1 + 9);
+
+        let der = A2::deserialize(&bytes).unwrap();
+        assert_eq!(ser.0, der.0);
+        assert_eq!(ser.1, der.1);
+    }
+}
