deba8b7a69afbf2b0806968906568328a6edd52d
SF-Zhou <sfzhou.scut@gmail.com>
2024-05-13 17:17:03 +0800

    derive support struct with lifetime (#9)

--- derse/tests/test_derive.rs
+++ derse/tests/test_derive.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use derse::{Derse, DownwardBytes, Serialization};
 
 #[test]
@@ -59,5 +61,21 @@ fn test_compatibility() {
         let der = A2::deserialize(&bytes).unwrap();
         assert_eq!(ser.0, der.0);
         assert_eq!(ser.1, der.1);
+    }
+}
+
+#[test]
+fn test_struct_with_lifetime() {
+    #[derive(Debug, Derse, PartialEq)]
+    struct A<'a>(Cow<'a, str>);
+
+    {
+        let ser = A(Cow::Owned("hello derse!".to_owned()));
+        let bytes = ser.serialize::<DownwardBytes>();
+        assert_eq!(bytes.len(), 1 + 1 + 12);
+
+        let der = A::deserialize(&bytes).unwrap();
+        assert!(matches!(der.0, Cow::Borrowed(_)));
+        assert_eq!(ser.0, der.0);
     }
 }
