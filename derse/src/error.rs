94e60b9a9ba8cfc920498b56923cd32f953f5e66
SF-Zhou <sfzhou.scut@gmail.com>
2024-06-21 09:36:56 +0800

    optimize error

--- derse/src/error.rs
+++ derse/src/error.rs
@@ -1,16 +1,31 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(thiserror::Error)]
 pub enum Error {
+    #[error("data is short for deserialize: expect {expect}, actual {actual}")]
     DataIsShort { expect: usize, actual: usize },
+    #[error("invalid bool {0}")]
     InvalidBool(u8),
-    InvalidString,
+    #[error("invalid string {0:?}")]
+    InvalidString(Vec<u8>),
+    #[error("varint is short")]
     VarintIsShort,
+    #[error("invalid type")]
     InvalidType,
 }
 
-impl std::fmt::Display for Error {
+impl std::fmt::Debug for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        std::fmt::Debug::fmt(self, f)
+        std::fmt::Display::fmt(self, f)
     }
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_error() {
+        println!("{:?}", Error::InvalidBool(233));
+    }
+}
