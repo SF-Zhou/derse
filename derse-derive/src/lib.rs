9ba20d6615e91b4e87264bbc9f755d7a94a2a473
SF-Zhou <sfzhou.scut@gmail.com>
2024-06-24 11:49:36 +0800

    support enum

--- derse-derive/src/lib.rs
+++ derse-derive/src/lib.rs
@@ -2,47 +2,88 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, Lifetime, LifetimeParam};
+use syn::{
+    parse_macro_input, Data, DataEnum, DataStruct, DeriveInput, Fields, Lifetime, LifetimeParam,
+};
 
 #[proc_macro_derive(serialize)]
 pub fn derse_serialize_derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
-    let name = &ast.ident;
-    let mut serialize_fields = Vec::new();
-    match ast.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => {
-            for f in fields.named {
-                let field_name = &f.ident;
-                serialize_fields.push(quote! {
-                    self.#field_name.serialize_to(serializer)?;
-                });
+    let struct_type = &ast.ident;
+    let statements = match ast.data {
+        Data::Struct(DataStruct { fields, .. }) => {
+            let mut idents = fields
+                .iter()
+                .enumerate()
+                .map(|(i, f)| {
+                    let index = syn::Index::from(i);
+                    f.ident
+                        .as_ref()
+                        .map_or(quote! {#index}, |ident| quote! {#ident})
+                })
+                .collect::<Vec<_>>();
+            idents.reverse();
+            quote! { #( self.#idents.serialize_to(serializer)?; )* }
+        }
+        Data::Enum(DataEnum { variants, .. }) => {
+            let mut match_statements = Vec::new();
+            for variant in variants {
+                let ident = &variant.ident;
+                let name = ident.to_string();
+                let match_statement = match variant.fields {
+                    Fields::Named(fields) => {
+                        let mut idents = fields.named.iter().map(|f| &f.ident).collect::<Vec<_>>();
+                        let list = quote! { #(#idents, )* };
+                        idents.reverse();
+                        quote! {
+                            Self::#ident { #list } => {
+                                #( #idents.serialize_to(serializer)?; )*
+                                #name.serialize_to(serializer)?;
+                            }
+                        }
+                    }
+                    Fields::Unnamed(fields) => {
+                        let mut idents = fields
+                            .unnamed
+                            .iter()
+                            .enumerate()
+                            .map(|(i, _)| {
+                                syn::Ident::new(&format!("v{i}"), proc_macro2::Span::call_site())
+                            })
+                            .collect::<Vec<_>>();
+                        let list = quote! { #(#idents, )* };
+                        idents.reverse();
+                        quote! {
+                            Self::#ident ( #list ) => {
+                                #( #idents.serialize_to(serializer)?; )*
+                                #name.serialize_to(serializer)?;
+                            }
+                        }
+                    }
+                    Fields::Unit => quote! {
+                        Self::#ident => {
+                            #name.serialize_to(serializer)?;
+                        }
+                    },
+                };
+                match_statements.push(match_statement);
+            }
+            quote! {
+                match self {
+                    #(#match_statements)*
+                }
             }
         }
-        Data::Struct(DataStruct {
-            fields: Fields::Unnamed(fields),
-            ..
-        }) => {
-            for (i, _) in fields.unnamed.iter().enumerate() {
-                let index = syn::Index::from(i);
-                serialize_fields.push(quote! {
-                    self.#index.serialize_to(serializer)?;
-                });
-            }
-        }
-        _ => panic!("only struct is supported"),
+        _ => panic!("only struct and enum are supported"),
     };
-    serialize_fields.reverse();
 
     quote! {
-        impl #impl_generics ::derse::Serialize for #name #ty_generics #where_clause {
+        impl #impl_generics ::derse::Serialize for #struct_type #ty_generics #where_clause {
             fn serialize_to<Serializer: ::derse::Serializer>(&self, serializer: &mut Serializer) -> ::derse::Result<()> {
                 let start = serializer.len();
-                #(#serialize_fields)*
+                #statements
                 let len = serializer.len() - start;
                 ::derse::VarInt64(len as u64).serialize_to(serializer)
             }
@@ -68,53 +109,76 @@ pub fn derse_deserialize_derive(input: TokenStream) -> TokenStream {
         (impl_generics, quote! { #lifetime })
     };
 
-    let name = &ast.ident;
-    let mut deserialize_fields = Vec::new();
-    let deserialize_statement = match ast.data {
-        Data::Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => {
-            for f in fields.named {
-                let field_name = &f.ident;
-                deserialize_fields.push(quote! {
-                    #field_name: if buf.is_empty() {
-                        Default::default()
-                    } else {
-                        ::derse::Deserialize::deserialize_from(&mut buf)?
-                    },
-                });
-            }
-            quote! {
-                Self {
-                    #(#deserialize_fields)*
-                }
-            }
-        }
-        Data::Struct(DataStruct {
-            fields: Fields::Unnamed(fields),
-            ..
-        }) => {
-            for _ in fields.unnamed {
-                deserialize_fields.push(quote! {
-                    if buf.is_empty() {
-                        Default::default()
-                    } else {
-                        ::derse::Deserialize::deserialize_from(&mut buf)?
-                    },
-                });
-            }
-            quote! {
-                Self (
-                    #(#deserialize_fields)*
-                )
+    let struct_type = &ast.ident;
+    let struct_name = struct_type.to_string();
+    let deserialize_statements = match ast.data {
+        Data::Struct(DataStruct { fields, .. }) => {
+            let statements = fields
+                .iter()
+                .map(|f| {
+                    let statement = quote! {
+                        if buf.is_empty() {
+                            Default::default()
+                        } else {
+                            ::derse::Deserialize::deserialize_from(&mut buf)?
+                        }
+                    };
+                    f.ident
+                        .as_ref()
+                        .map_or(statement.clone(), |ident| quote! {#ident: #statement})
+                })
+                .collect::<Vec<_>>();
+            match fields {
+                Fields::Named(_) => quote! { let result = Self { #(#statements, )* }; },
+                Fields::Unnamed(_) => quote! { let result = Self ( #(#statements, )* ); },
+                Fields::Unit => quote! { let result = Self; },
             }
         }
-        _ => panic!("only struct is supported"),
+        Data::Enum(DataEnum { variants, .. }) => {
+            let mut match_statements = Vec::new();
+            for variant in variants {
+                let ident = &variant.ident;
+                let variant_name = ident.to_string();
+                let statements = variant
+                    .fields
+                    .iter()
+                    .map(|f| {
+                        let statement = quote! {
+                            if buf.is_empty() {
+                                Default::default()
+                            } else {
+                                ::derse::Deserialize::deserialize_from(&mut buf)?
+                            }
+                        };
+                        f.ident
+                            .as_ref()
+                            .map_or(statement.clone(), |ident| quote! {#ident: #statement})
+                    })
+                    .collect::<Vec<_>>();
+                let match_statement = match variant.fields {
+                    Fields::Named(_) => {
+                        quote! { #variant_name => Self::#ident { #(#statements, )* }, }
+                    }
+                    Fields::Unnamed(_) => {
+                        quote! { #variant_name => Self::#ident ( #(#statements, )* ), }
+                    }
+                    Fields::Unit => quote! { #variant_name => Self::#ident, },
+                };
+                match_statements.push(match_statement);
+            }
+            quote! {
+                let ty = <&str>::deserialize_from(&mut buf)?;
+                let result = match ty {
+                    #(#match_statements)*
+                    _ => return Err(::derse::Error::InvalidType(format!("{}::{}", #struct_name, ty))),
+                };
+            }
+        }
+        _ => panic!("only struct and enum are supported"),
     };
 
     quote! {
-        impl #impl_generics #name #ty_generics #where_clause {
+        impl #impl_generics #struct_type #ty_generics #where_clause {
             pub fn deserialize_and_split<Deserializer: ::derse::Deserializer<#lifetime>>(mut b: Deserializer) -> ::derse::Result<(Self, Deserializer)>
             where
                 Self: Sized,
@@ -123,18 +187,20 @@ pub fn derse_deserialize_derive(input: TokenStream) -> TokenStream {
                 let mut buf = &mut b;
                 let len = ::derse::VarInt64::deserialize_from(buf)?.0 as usize;
                 let mut buf = buf.advance(len)?;
-                Ok((#deserialize_statement, buf))
+                #deserialize_statements
+                Ok((result, buf))
             }
         }
 
-        impl #impl_generics ::derse::Deserialize<#lifetime> for #name #ty_generics #where_clause {
+        impl #impl_generics ::derse::Deserialize<#lifetime> for #struct_type #ty_generics #where_clause {
             fn deserialize_from<Deserializer: ::derse::Deserializer<#lifetime>>(buf: &mut Deserializer) -> ::derse::Result<Self>
             where
                 Self: Sized,
             {
                 let len = ::derse::VarInt64::deserialize_from(buf)?.0 as usize;
                 let mut buf = buf.advance(len)?;
-                Ok(#deserialize_statement)
+                #deserialize_statements
+                Ok(result)
             }
         }
     }.into()
